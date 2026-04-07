4083b8b5b760a4831d30e0ccefcf2c17d888aaf7
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-07 18:37:06 +0700

    menambahkan tampilan data setelah pengisian

--- struct.cpp
+++ struct.cpp
@@ -8,3 +8,23 @@ struct orang
     int umur;
 };
 
+int main()
+{
+    // deklarasi variable struct
+    orang mhs;
+
+    // mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "Isikan data usia : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+    cout << endl;
+}
