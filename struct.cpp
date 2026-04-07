b6be81078b3c6eeabe0f94c6166b4f20bcdc7529
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-04-07 18:34:56 +0700

    menambah file struct dan membuat library

--- struct.cpp
+++ struct.cpp
@@ -1,30 +1,3 @@
 #include <iostream>
 using namespace std;
 
-struct orang
-{
-    string nama;
-    string alamat;
-    int umur;
-};
-
-int main()
-{
-    // deklarasi variable struct
-    orang mhs;
-
-    // mengisi data
-    cout << "Isikan data nama : ";
-    cin >> mhs.nama;
-    cout << "Isikan data alamat : ";
-    cin >> mhs.alamat;
-    cout << "Isikan data usia : ";
-    cin >> mhs.umur;
-    cout << endl;
-
-    cout << "Data tampil" << endl;
-    cout << "Nama : " << mhs.nama << endl;
-    cout << "Alamat : " << mhs.alamat << endl;
-    cout << "Umur : " << mhs.umur << endl;
-    cout << endl;
-}
